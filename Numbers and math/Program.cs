b3e4b4185adeb7ee8b387ea3dd8ed711b3371288
Nathan Barnes <339723215@GoTVDSB.ca>
2024-09-11 09:39:29 -0400

    Add project files.

---
+++ Numbers and math/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Numbers_and_math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //Nate Barnes
+
+
+
+            Console.WriteLine("I will now count my chickens");
+            Console.WriteLine("Hens" + (10 + 5 * 2));
+            Console.WriteLine("Roosters" + (25 + 30 / 6));
+            Console.WriteLine("six" + 6);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("I will now count my eggs:");
+            Console.WriteLine(11.0 / 2);
+            Console.WriteLine();
+            Console.WriteLine(" Is it true that 3 + 2 > 5 - 7?");
+            Console.WriteLine(3 + 2 > 5 - 7);
+            Console.WriteLine("What is 3 + 2? " + (3 + 2));
+            Console.WriteLine("Oh, thats why it's " + (3 + 2 > 5 - 7));
+            //Putting the equation in double quotes asks the comuter a question that should be anwered with text, instead of numbers.
+            Console.WriteLine();
+            Console.WriteLine("What is 8 * 21? " + (8 * 21).ToString("C");
+            
+
+
+        }
+
+
+    }
+}
